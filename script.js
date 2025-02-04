d1214431ec6dfc2115ac702cbe94ce1f5ea8d4fe
flipmapping <flipmapping@gmail.com>
2025-02-04 18:01:08 +0700

    unpdates

--- script.js
+++ script.js
@@ -3,19 +3,12 @@
 js
 
 document.addEventListener("DOMContentLoaded", function() {
-
     const accordionHeaders = document.querySelectorAll(".accordion-header");
 
     accordionHeaders.forEach(header => {
-
         header.addEventListener("click", function() {
-
             const content = this.nextElementSibling;
-
             content.style.display = content.style.display === "block" ? "none" : "block";
-
         });
-
     });
-
 });
